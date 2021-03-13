4f1e9cef419afb49a6fb79a9c5abbfd373728c86
vnvj0033 <vnvj0033@gmail.com>
2021-03-13 23:31:26 +0900

    06-09 여름과목 테스트 작성

---
+++ test/summer/SummerCourseSessionTest.java
@@ -0,0 +1,20 @@
+package summer;
+
+import org.junit.jupiter.api.Test;
+import studentInfo.CourseSession;
+import studentInfo.DateUtil;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SummerCourseSessionTest {
+
+    @Test
+    void testEndDate(){
+        Date startDate = DateUtil.createDate(2003, 6, 9);
+        CourseSession session = SummerCourseSession.creat("ENGL", "200", startDate);
+        Date eightWeeksOut = DateUtil.createDate(2003, 8, 1);
+        assertEquals(eightWeeksOut, session.getEndDate());
+    }
+}
