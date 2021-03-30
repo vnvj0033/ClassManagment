e0e8f883601d49ad54ab4cddcd5c3671496394bb
vnvj0033 <vnvj0033@gmail.com>
2021-03-30 21:44:01 +0900

    08-02 Add. Scorer에 isValid와 테스트

--- src/studentInfo/Scorer.java
+++ src/studentInfo/Scorer.java
@@ -4,4 +4,13 @@ public class Scorer {
     public int score(String input) {
         return Integer.parseInt(input);
     }
+
+    public boolean isValid(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        }catch (NumberFormatException e){
+            return false;
+        }
+    }
 }
--- test/studentInfo/ScorerTest.java
+++ test/studentInfo/ScorerTest.java
@@ -2,8 +2,7 @@ package studentInfo;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ScorerTest {
     @Test
@@ -20,6 +19,13 @@ public class ScorerTest {
             fail("expected NumberFormatException on bad input");
         }catch (NumberFormatException success){
         }
+    }
+
+    @Test
+    void testIsValid() {
+        Scorer scorer = new Scorer();
+        assertTrue(scorer.isValid("75"));
+        assertFalse(scorer.isValid("bd"));
     }
 
 }
