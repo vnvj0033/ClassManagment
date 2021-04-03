01483102faa88c5fcef6e067ee0496c84a487099
vnvj0033 <vnvj0033@gmail.com>
2021-04-03 21:31:59 +0900

    09-06 Add. Course equals and test

--- src/studentInfo/Course.java
+++ src/studentInfo/Course.java
@@ -1,11 +1,12 @@
 package studentInfo;
 
+
 public class Course {
 
     private String department;
     private String number;
 
-    public Course(String department, String number){
+    public Course(String department, String number) {
         this.department = department;
         this.number = number;
     }
@@ -16,5 +17,13 @@ public class Course {
 
     public String getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null)
+            return false;
+        Course that = (Course) object;
+        return this.department.equals(that.department) && this.number.equals(that.number);
     }
 }
--- test/studentInfo/CourseTest.java
+++ test/studentInfo/CourseTest.java
@@ -3,6 +3,7 @@ package studentInfo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CourseTest {
 
@@ -11,5 +12,33 @@ public class CourseTest {
         Course course = new Course("CMSC", "120");
         assertEquals("CMSC", course.getDepartment());
         assertEquals("120", course.getNumber());
+    }
+
+    @Test
+    void testEquality() {
+        Course courseA = new Course("NURS", "201");
+        Course courseAPrime = new Course("NURS", "201");
+        assertEquals(courseA, courseAPrime);
+
+        Course courseB = new Course("ARTH", "330");
+        assertFalse(courseA.equals(courseB));
+
+        // reflexivity
+        assertEquals(courseA, courseA);
+
+        //transitivity
+        Course courseAPrime2 = new Course("NURS", "201");
+        assertEquals(courseAPrime, courseAPrime2);
+        assertEquals(courseA, courseAPrime2);
+
+        // symmetry
+        assertEquals(courseAPrime, courseA);
+
+        //consistency
+        assertEquals(courseA, courseAPrime);
+
+        // comparison to null
+        assertFalse(courseA.equals(null));
+
     }
 }
