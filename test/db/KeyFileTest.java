18ffb01ac5540d43717482152cce167259e31c3c
vnvj0033 <vnvj0033@gmail.com>
2021-04-11 21:50:28 +0900

    11-17 Add. KeyFileTest

---
+++ test/db/KeyFileTest.java
@@ -0,0 +1,50 @@
+package db;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class KeyFileTest {
+    private static final String FILENAME = "keyfiletest.idx";
+    private static final String KEY = "key";
+    private static final long POSITION = 1;
+    private static final int LENGTH = 100;
+
+    private KeyFile keyFile;
+
+    @BeforeEach
+    protected void setUp() {
+        TestUtil.delete(FILENAME);
+        keyFile = new KeyFile(FILENAME);
+    }
+
+    protected void tearDown() throws IOException {
+        assertEquals(0, keyFile.size());
+    }
+
+    @Test
+    public void testAddEntry() {
+        keyFile.add(KEY, POSITION, LENGTH);
+
+        assertEquals(1, keyFile.size());
+        assertTrue(keyFile.constainsKey(KEY));
+        assertEquals(POSITION, keyFile.getPosition(KEY));
+        assertEquals(LENGTH, keyFile.getLength(KEY));
+    }
+
+    @Test
+    public void testReopen() {
+        keyFile.add(KEY, POSITION, LENGTH);
+        keyFile.close();
+
+        keyFile = new KeyFile(FILENAME);
+        assertEquals(1, keyFile.size());
+        assertEquals(POSITION, keyFile.getPosition(KEY));
+        assertEquals(LENGTH, keyFile.getLength());
+    }
+
+}
