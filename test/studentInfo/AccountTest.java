4caf32567565ebac862efba9abc955c7597fd840
vnvj0033 <vnvj0033@gmail.com>
2021-04-12 21:53:29 +0900

    12-01 Fix. AccountTest use jimbob.ach api

--- test/studentInfo/AccountTest.java
+++ test/studentInfo/AccountTest.java
@@ -1,5 +1,6 @@
 package studentInfo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -7,6 +8,19 @@ import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTest {
+    static final String ABA = "102000012";
+    static final String ACCOUNT_NUMBER = "194431518811";
+
+    private Account account;
+
+    @BeforeEach
+    void setUp() {
+        account = new Account();
+        account.setBankAba(ABA);
+        account.setBankAccountNumber(ACCOUNT_NUMBER);
+        account.setBankAccountType(Account.BankAccountType.CHECKING);
+    }
+
     @Test
     void testTransactions() {
         Account account = new Account();
@@ -14,5 +28,14 @@ public class AccountTest {
         account.credit(new BigDecimal("11.00"));
         account.credit(new BigDecimal("2.99"));
         assertEquals(new BigDecimal("4.70"), account.transactionAverage());
+    }
+
+    @Test
+    void testTransferFromBank() {
+        account.setAch(new com.jimbob.ach.JimBobAch());
+        final BigDecimal amount = new BigDecimal("50.00");
+        account.transferFormBank(amount);
+
+        assertEquals(amount, account.getBalance());
     }
 }
