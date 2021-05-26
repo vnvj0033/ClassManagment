3177a777cfcd18697ea1ace69d4797d404dd041b
sangyeop yoo <vnvj0033@gmail.com>
2021-05-26 10:31:19 +0900

    1-2 swing start

---
+++ src/ui/Sis.java
@@ -0,0 +1,30 @@
+package ui;
+
+import javax.swing.*;
+
+public class Sis {
+    static final int WIDTH = 300;
+    static final int HEIGHT = 200;
+    private JFrame frame = new JFrame();
+
+    public static void main(String[] args) {
+        new Sis().show();
+    }
+
+    Sis() {
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void show() {
+        frame.setVisible(true);
+    }
+
+    JFrame getFrame() {
+        return frame;
+    }
+
+    void close() {
+        frame.dispose();
+    }
+}
---
+++ test/ui/SisTest.java
@@ -0,0 +1,40 @@
+package ui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SisTest {
+    private Sis sis;
+    private JFrame frame;
+
+    @BeforeEach
+    protected void setUp() {
+        sis = new Sis();
+        frame = sis.getFrame();
+    }
+
+    @Test
+    public void testCreate() {
+        final double tolerance = 0.05;
+        assertEquals(Sis.HEIGHT, frame.getSize().getHeight(), tolerance);
+        assertEquals(Sis.WIDTH, frame.getSize().getWidth(), tolerance);
+        assertEquals(JFrame.EXIT_ON_CLOSE, frame.getDefaultCloseOperation());
+    }
+
+    @Test
+    public void testShow() {
+        sis.show();
+        assertTrue(frame.isVisible());
+    }
+
+    @AfterEach
+    protected void tearDown() {
+        sis.close();
+    }
+}
