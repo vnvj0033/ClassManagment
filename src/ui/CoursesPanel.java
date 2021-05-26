b4b8d8d7adfea62953325744a92ac6174c0a1d5f
sangyeop yoo <vnvj0033@gmail.com>
2021-05-26 16:54:27 +0900

    1-6 swing widget

--- src/ui/CoursesPanel.java
+++ src/ui/CoursesPanel.java
@@ -3,9 +3,18 @@ package ui;
 import javax.swing.*;
 
 public class CoursesPanel extends JPanel {
-    public static final String LABEL_NAME = "coursesLabel";
     public static final String NAME = "coursesPanel";
     public static final String LABEL_TEXT = "Courses";
+    public static final String LABEL_NAME = "coursesLabel";
+    public static final String COURSES_LIST_NAME = "coursesList";
+    public static final String ADD_BUTTON_TEXT = "Add";
+    public static final String ADD_BUTTON_NAME = "addButton";
+    public static final String DEPARTMENT_FIELD_NAME = "deptField";
+    public static final String NUMBER_FIELD_NAME = "numberField";
+    public static final String DEPARTMENT_LABEL_NAME = "deptLabel";
+    public static final String NUMBER_LABEL_NAME = "numberLabel";
+    public static final String DEPARTMENT_LABEL_TEXT = "Department";
+    public static final String NUMBER_LABEL_TEXT = "Number";
 
     public static void main(String[] args) {
         show(new CoursesPanel());
@@ -13,7 +22,7 @@ public class CoursesPanel extends JPanel {
 
     private static void show(JPanel panel) {
         JFrame frame = new JFrame();
-        frame.setSize(100, 100);
+        frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(panel);
         frame.setVisible(true);
@@ -27,6 +36,33 @@ public class CoursesPanel extends JPanel {
     private void createLayout() {
         JLabel label = new JLabel(LABEL_TEXT);
         label.setName(LABEL_NAME);
+
+        JList list = new JList();
+        list.setName(COURSES_LIST_NAME);
+
+        JButton addButton = new JButton(ADD_BUTTON_TEXT);
+        addButton.setName(ADD_BUTTON_NAME);
+
+        int columns = 20;
+
+        JLabel departmentLabel = new JLabel(DEPARTMENT_LABEL_TEXT);
+        departmentLabel.setName(DEPARTMENT_LABEL_NAME);
+
+        JTextField departmentField = new JTextField(columns);
+        departmentField.setName(DEPARTMENT_FIELD_NAME);
+
+        JLabel numberLabel = new JLabel(NUMBER_LABEL_TEXT);
+        numberLabel.setName(NUMBER_LABEL_NAME);
+
+        JTextField numberField = new JTextField(columns);
+        numberField.setName(NUMBER_FIELD_NAME);
+
         add(label);
+        add(list);
+        add(addButton);
+        add(departmentLabel);
+        add(departmentField);
+        add(numberLabel);
+        add(numberField);
     }
 }
--- test/ui/CoursesPanelTest.java
+++ test/ui/CoursesPanelTest.java
@@ -5,13 +5,32 @@ import org.junit.jupiter.api.Test;
 import javax.swing.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ui.CoursesPanel.*;
 
 public class CoursesPanelTest {
 
     @Test
     public void testCreate() {
         CoursesPanel panel = new CoursesPanel();
-        JLabel label = (JLabel) Util.getComponent(panel, CoursesPanel.NAME);
-        assertEquals(CoursesPanel.LABEL_TEXT, label.getText());
+        JLabel label = (JLabel) Util.getComponent(panel, LABEL_NAME);
+        assertEquals(LABEL_TEXT, label.getText());
+
+        JList list = (JList) Util.getComponent(panel, COURSES_LIST_NAME);
+        assertEquals(0, list.getModel().getSize());
+
+        JButton button = (JButton) Util.getComponent(panel, ADD_BUTTON_NAME);
+        assertEquals(ADD_BUTTON_TEXT, button.getText());
+
+        JLabel departmentLabel = (JLabel) Util.getComponent(panel, DEPARTMENT_LABEL_NAME);
+        assertEquals(DEPARTMENT_LABEL_TEXT, departmentLabel.getText());
+
+        JTextField departmentField = (JTextField) Util.getComponent(panel, DEPARTMENT_FIELD_NAME);
+        assertEquals("", departmentField.getText());
+
+        JLabel numberLabel = (JLabel) Util.getComponent(panel, NUMBER_LABEL_NAME);
+        assertEquals(NUMBER_LABEL_TEXT, numberLabel.getText());
+
+        JTextField numberField = (JTextField) Util.getComponent(panel, NUMBER_FIELD_NAME);
+        assertEquals("", numberField.getText());
     }
 }
